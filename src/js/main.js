b69c5bf0557398b0cc75f83d6657f38815bb9466
adshdz <andress.hernandezp@ecci.edu.co>
2022-08-23 11:50:02 -0500

    ajuste pantalla

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 
 const datosForm = document.querySelector("input.submit");
 
+
+const email = document.querySelector("input.emailForm");
+const parrafo = document.querySelector("div.errores ul")
+const pass  = document.querySelector("input.passForm");
+const arrayErrores = [];
+
 datosForm.addEventListener("click", function(e){
     e.preventDefault();
-    const email = document.querySelector("input.emailForm");
-    const parrafo = document.querySelector("div.errores ul")
-    const pass  = document.querySelector("input.passForm");
-    const arrayErrores = [];
+    
 
     if(email.value == ""){
         arrayErrores.push("El campo email esta vacio");
@@ -15,8 +18,28 @@ datosForm.addEventListener("click", function(e){
         arrayErrores.push("El campo pass esta vacio");
     }
     if(arrayErrores.length>0){
+        
         for( let i = 0; i<arrayErrores.length; i++){
             parrafo.innerHTML += "<li>" + arrayErrores[i] + "</li>"
         }
+
+    }
+// se busca dejar en blanco los mensajes de errores
+    setTimeout(()=>{
+    parrafo.outerHTML = "";
+}, 2000)
+});
+
+email.addEventListener('input', function(e) {
+    campo = e.target;
+    valido = document.getElementById('emailOK');
+        
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+      if (emailRegex.test(campo.value)) {
+      valido.innerText = "válido";
+    } else {
+      valido.innerText = "incorrecto";
     }
 });
+
+
