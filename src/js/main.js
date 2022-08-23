8bae063d5bc98b67a7fcf3df8074f26bbb5017a6
adshdz <andress.hernandezp@ecci.edu.co>
2022-08-23 16:47:03 -0500

    ajuste pantalla

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,44 @@
 
 const datosForm = document.querySelector("input.submit");
 const email = document.querySelector("input.emailForm");
+const pass = document.querySelector("input.passForm");
 const parrafo = document.querySelector("div.errores ul")
-const pass  = document.querySelector("input.passForm");
 const arrayErrores = [];
 
-datosForm.addEventListener("click", function(e){   
-   
-
-    if(email.value == ""){
+datosForm.addEventListener("click", function (e) {
+    if (email.value == "") {
         arrayErrores.push("El campo email esta vacio");
     }
-    if(pass.value == ""){
+    if (pass.value == "") {
         arrayErrores.push("El campo contraseña esta vacio");
     }
-    if(arrayErrores.length>0){
+    if (arrayErrores.length > 0) {
         e.preventDefault();
-        for( let i = 0; i<arrayErrores.length; i++){
-            parrafo.innerHTML += `<li id ="remove${i}">${arrayErrores[i]}</li>`
-            
-            
+        for (let i = 0; i < arrayErrores.length; i++) {
+            parrafo.innerHTML += `<li id ="remove">  ${arrayErrores[i]}</li>`
         }
-
+   remover("remove");
     }
-// se busca dejar en blanco los mensajes de errores
-    setTimeout(()=>{
-        for( let i = 0; i<arrayErrores.length; i++){
-        document.getElementById("remove"+i).remove();
-        }
-}, 2000)
 });
 
+function remover(id) {
+    setTimeout(() => {
+        for (let i = 0; i < arrayErrores.length; i++) {
+            document.getElementById(id).remove();
+        }
+        for (let i = 0; i <= arrayErrores.length; i++) {
+            arrayErrores.pop();
+        }
+    }, 2000)
+}
 
-email.addEventListener('input', function(e) {
+email.addEventListener('input', function (e) {
     campo = e.target;
     valido = document.getElementById('emailOK');
-        
     emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-      if (emailRegex.test(campo.value)) {
-      valido.innerText = " correo válido";
+    if (emailRegex.test(campo.value)) {
+        valido.innerText = " correo válido";
     } else {
-      valido.innerText = "incorrecto";
+        valido.innerText = "incorrecto";
     }
 });
-
-
